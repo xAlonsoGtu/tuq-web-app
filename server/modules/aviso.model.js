07884b1e1dbb14e1d3bf70cf90a6efb57aa5ad91
brenda <bs094716@gmail.com>
2026-01-31 11:38:21 -0600

    brendysinicio

---
+++ server/modules/aviso.model.js
@@ -0,0 +1,11 @@
+export class Avisos {
+  aviso_id;         
+  nombre;           
+  descripcion;     
+  imagen_url;     
+  fecha_inicio;    
+  status;          
+  created_at;      
+  updated_at;       
+  deleted_at;       
+}
