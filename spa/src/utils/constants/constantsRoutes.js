23308e5abc4cde8fc79f5cfcf0ff51e3ee75ea89
hugosani <hugosani020301@hotmail.com>
2026-02-21 10:49:00 -0600

    front empleado

--- spa/src/utils/constants/constantsRoutes.js
+++ spa/src/utils/constants/constantsRoutes.js
@@ -7,6 +7,9 @@ export class ConstantsRoutes {
     static get SPA_ADMIN_HOME() { return "/admin/panel"; };
     static get SPA_MAESTRO_LISTAR() { return "/admin/maestro"; };
     static get SPA_MAESTRO_AGREGAR() { return "/admin/maestro/agregar"; };
+  static get SPA_EMPLEADO_LISTAR() { return "/admin/empleado"; };
+    static get SPA_EMPLEADO_AGREGAR() { return "/admin/empleado/agregar"; };
+    static get SPA_EMPLEADO_EDITAR() { return "/admin/empleado/editar"; };
 
     /* API ROUTES */
     /* AUTH */
@@ -19,4 +22,14 @@ export class ConstantsRoutes {
     static get MAESTRO_ELIMINAR() { return "/api/maestro/eliminar"; };
     static get MAESTRO_OBTENER() { return "/api/maestro/obtener"; };
     static get MAESTRO_BUSCAR() { return "/api/maestro/buscar"; };
+        /* API ROUTES */
+    static get EMPLEADO_AGREGAR() { return "/api/empleado/agregar"; };
+    static get EMPLEADO_EDITAR() { return "/api/empleado/editar"; };
+    static get EMPLEADO_EDITAR_STATUS() { return "/api/empleado/status"; };
+    static get EMPLEADO_ELIMINAR() { return "/api/empleado/eliminar"; };
+    static get EMPLEADO_OBTENER() { return "/api/empleado/obtener"; };
+    static get EMPLEADO_BUSCAR() { return "/api/empleado/buscar"; };
 }
+
+
+
